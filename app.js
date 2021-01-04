ed103aadcc1a99f983f93447dcaf7c0a5a0e52f0
JIN-SKILL <huangjinhu.1995@gmail.com>
2021-01-04 17:35:25 +0800

    add update todo function

--- app.js
+++ app.js
@@ -35,6 +35,7 @@ app.get('/', (req, res) => {
     .catch(error => console.log(error)) // 如果發生意外, 執行錯誤處理
 })
 
+// CRUD create
 app.get('/todos/new', (req, res) => {
   return res.render('new')
 })
@@ -46,6 +47,7 @@ app.post('/todos', (req, res) => {
     .catch(error => console.log(error))
 })
 
+// CRUD read
 app.get('/todos/:id', (req, res) => {
   const id = req.params.id
   return Todo.findById(id)
@@ -53,6 +55,29 @@ app.get('/todos/:id', (req, res) => {
     .then(todo => res.render('detail', { todo }))
     .catch(error => console.log(error))
 })
+
+// CRUD update
+app.get('/todos/:id/edit', (req, res) => {
+  const id = req.params.id
+  return Todo.findById(id)
+    .lean()
+    .then(todo => res.render('edit', { todo }))
+    .catch(error => console.log(error))
+})
+
+app.post('/todos/:id/edit', (req, res) => {
+  const id = req.params.id
+  const name = req.body.name
+  return Todo.findById(id)
+    .then(todo => {
+      todo.name = name
+      return todo.save()
+    })
+    .then(() => res.redirect(`/todos/${id}`))
+    .catch(error => console.log(error))
+})
+
+// CRUD delete
 
 // listen
 app.listen(port, () => {
