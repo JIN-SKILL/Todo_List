f9348b110d36d91335967fe908d5def1f8bc0562
JIN-SKILL <huangjinhu.1995@gmail.com>
2021-01-04 10:24:11 +0800

    add mongoose

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
 const port = 3000
+
+mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true,  useUnifiedTopology: true })
+
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+
+db.once('open', () => {
+  console.log('moongodb connected!')
+})
 
 app.get('/', (req, res) => {
   res.send('hello world')
 })
 
 app.listen(port, () => {
-  console.log(`The server is running in htpp://localhost:${port}`)
+  console.log(`The server is running in http://localhost:${port}`)
 })
