f7376f33d42b8492517f0c148513f743da432f7c
JIN-SKILL <huangjinhu.1995@gmail.com>
2021-01-07 07:52:02 +0800

    refactor: modify express routes

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
+const routes = require('./routes')
 const Todo = require('./models/todo')
 const app = express()
 const port = 2900
@@ -29,66 +30,7 @@ db.once('open', () => {
 })
 
 // setting route
-app.get('/', (req, res) => {
-  // get all todo data from db
-  Todo.find() // 從資料庫查找出資料
-    .lean() // 把資料轉成單純的JS物件
-    .sort({ _id: 'asc' })
-    .then(todos => res.render('index', { todos })) // 然後把資料送給前端樣版
-    .catch(error => console.log(error)) // 如果發生意外, 執行錯誤處理
-})
-
-// CRUD create
-app.get('/todos/new', (req, res) => {
-  return res.render('new')
-})
-
-app.post('/todos', (req, res) => {
-  const name = req.body.name           // 從 req.body 拿出表單裡的 name 資料
-  return Todo.create({ name })         // 存入資料庫
-    .then(() => res.redirect('/'))     // 新增完成後導回首頁
-    .catch(error => console.log(error))
-})
-
-// CRUD read
-app.get('/todos/:id', (req, res) => {
-  const id = req.params.id
-  return Todo.findById(id)
-    .lean()
-    .then(todo => res.render('detail', { todo }))
-    .catch(error => console.log(error))
-})
-
-// CRUD update
-app.get('/todos/:id/edit', (req, res) => {
-  const id = req.params.id
-  return Todo.findById(id)
-    .lean()
-    .then(todo => res.render('edit', { todo }))
-    .catch(error => console.log(error))
-})
-
-app.put('/todos/:id', (req, res) => {
-  const id = req.params.id
-  const { name, isDone } = req.body
-  return Todo.findById(id)
-    .then(todo => {
-      todo.name = name
-      todo.isDone = isDone === 'on'
-      return todo.save()
-    })
-    .then(() => res.redirect(`/todos/${id}`))
-    .catch(error => console.log(error))
-})
-
-// CRUD delete
-app.delete('/todos/:id', (req, res) => {
-  const id = req.params.id
-  return Todo.findById(id)
-    .then(todo => todo.remove())
-    .then(() => res.redirect('/'))
-    .catch(error => console.log(error))
-})
+app.use(routes)
 
 
 // listen
